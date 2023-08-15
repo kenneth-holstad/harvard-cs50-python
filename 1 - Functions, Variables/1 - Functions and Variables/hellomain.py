ddcd849b36ec27a6b149c5ae16e09be57189288f
Kenneth Holstad <kenneth-holstad@gmail.com>
2023-08-15 03:21:13 -0400

    Add hellomain code

---
+++ 1 - Functions, Variables/1 - Functions and Variables/hellomain.py
@@ -0,0 +1,14 @@
+# show general scripting convention
+# also add conditional to allow default behavior
+def main():
+    name = input("Name?" )
+    if name.strip() == "":
+        hello()
+    else:
+        hello(name)
+
+# note that you CANNOT call name in this function because main() exists only in main()
+def hello(to="world"):
+    print("hello,", to)
+
+main()
